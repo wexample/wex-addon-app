d125111ae1627286e66bee6b0c828abc30501714
weeger <romain.weeger@gmail.com>
2022-12-14 17:07:04 +0100

    service/user

---
+++ bash/service/used.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+serviceUsedArgs() {
+  _ARGUMENTS=(
+    'service s "Service to install" true'
+  )
+}
+
+serviceUsed() {
+  local SERVICES=("$(wex service/list)")
+
+  # Array contains
+  if [[ " ${SERVICES[*]} " =~ ${SERVICE} ]]; then
+    echo true
+  else
+    echo false
+  fi
+}
