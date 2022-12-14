69d4bce99debf4d56721c7c96c80d01c1b0a5a8b
weeger <romain.weeger@gmail.com>
2022-12-14 16:34:11 +0100

    app/locate

---
+++ bash/app/locate.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+appLocateArgs() {
+  _DESCRIPTION="Search into parent tree if current folder is in a wex project. Returns the wex project real path if found"
+  _ARGUMENTS=(
+    'dir d "Starting directory" false'
+  )
+}
+
+appLocate() {
+  local DIR_PREVIOUS
+  local DIR_CURRENT
+  local DIR_ORIGINAL
+  local DIR_WEX
+
+  if [[ "${DIR}" != "" ]];then
+    DIR_CURRENT="${DIR}"
+  else
+    DIR_CURRENT="$(realpath .)"
+  fi
+
+  DIR_ORIGINAL=${DIR_CURRENT}
+
+  while [ "${DIR_CURRENT}" != "${DIR_PREVIOUS}" ]; do
+    DIR_WEX="${DIR_CURRENT}/${WEX_FILE_APP_FOLDER}/"
+
+    # Config found.
+    if [ -f "${DIR_WEX}${WEX_FILE_APP_CONFIG}" ];then
+      echo "${DIR_CURRENT}/"
+      return
+    fi
+
+    DIR_PREVIOUS=${DIR_CURRENT}
+    DIR_CURRENT=$(realpath "${DIR_CURRENT}/../")
+  done
+
+  cd "${DIR_ORIGINAL}"
+}
---
+++ tests/bash/app/locate.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+appLocateTest() {
+  # Try to locate an dir outside a wex app.
+  _wexTestAssertEqual "$(wex app/locate -d="${WEX_DIR_TMP}")" ""
+}
