9eeff9c7943b5b74df42c4cbd491f287ae4b55ef
weeger <romain.weeger@gmail.com>
2023-01-25 22:08:46 +0100

    destroy

--- bash/app/destroy.sh
+++ bash/app/destroy.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 appDestroy() {
+  _wexAppGoTo
+
+  # Ensure app is stopped
+  wex app/stop
+
+  # Execute hooks.
   wex app::hook/exec -c=appDestroy
-  # Removing ket method does not support custom separator for now.
+
+  # Removing key method does not support custom separator for now.
   wex app::config/setValue -b -k=APP_INITIALIZED -v=false
 }
