243044114a41db2573acb7cb800470ad76988e7a
weeger <romain.weeger@gmail.com>
2023-02-01 09:08:31 +0100

    Various minor improvements

--- bash/config/setValue.sh
+++ bash/config/setValue.sh
@@ -6,6 +6,7 @@ configSetValueArgs() {
    'separator s "Separator like space or equal sign, default space" false " "'
    'ignore_duplicates i "Do not check if variable exists or is commented" false false'
    'value v "New value" true'
+   'verbose vv "Show log message" false false'
    'base b "Use base configuration file" false'
   )
 }
@@ -19,5 +20,5 @@ configSetValue() {
     FILE=${WEX_FILEPATH_REL_CONFIG_BUILD}
   fi
 
-  wex default::config/setValue -f="${FILE}" -s="=" -k="${KEY}" -i="${IGNORE_DUPLICATES}" -v="${VALUE}"
+  wex default::config/setValue -f="${FILE}" -s="=" -k="${KEY}" -i="${IGNORE_DUPLICATES}" -v="${VALUE}" -vv="${VERBOSE}"
 }
