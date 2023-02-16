848fbced7c159d191ac64b445b7a3abfd86edbd6
weeger <romain.weeger@gmail.com>
2023-02-16 20:12:51 +0100

    app/locate

--- tests/bash/app/locate.sh
+++ tests/bash/app/locate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 appLocateTest() {
-  # Try to locate an dir outside a wex app.
-  _wexTestAssertEqual "$(wex app::app/locate -d="${WEX_DIR_TMP}")" ""
+  # Try to locate an app outside wex temp dir.
+  _wexTestAssertEqual "$(wex app::app/locate -d="${WEX_DIR_TMP}")" "$(realpath "${WEX_DIR_ROOT}")/"
 }
