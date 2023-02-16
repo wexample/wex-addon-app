9cffcdacda9eaeea8b3dcadc7e1493ab48dbfee8
weeger <romain.weeger@gmail.com>
2023-02-16 19:59:43 +0100

    Minor

--- tests/bash/app/isset.sh
+++ tests/bash/app/isset.sh
@@ -2,5 +2,6 @@
 
 appIssetTest() {
   # Try to locate an dir outside a wex app.
-  _wexTestAssertEqual "$(wex app::app/isset -d="${WEX_DIR_TMP}")" "false"
+  # Wex core is an app
+  _wexTestAssertEqual "$(wex app::app/isset -d="${WEX_DIR_TMP}")" "true"
 }
