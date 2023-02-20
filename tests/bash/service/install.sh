6bae7bc45bc2166d2914264e798791a193fe7911
weeger <romain.weeger@gmail.com>
2023-02-20 21:12:02 +0100

    Recreate app for each tested service

--- tests/bash/service/install.sh
+++ tests/bash/service/install.sh
@@ -4,26 +4,25 @@ serviceInstallTest() {
   # Stop sites if exists
   wex-exec docker/stopAll
 
-  # Clear dir.
-  _wexTestClearTempDir
-
-  # Create a new test app for reference,
-  # it will be used to compare with other test apps after making changes.
-  _appTest_createApp "test-app-ref"
-
-  # Create a new test app if not exists.
-  _appTest_createApp "test-app"
-
-  # Go to fist site
-  cd "${WEX_TEST_DIR_TMP}test-app"
-
   local SERVICES
   SERVICES=$(wex-exec app::services/all)
 
   for SERVICE in ${SERVICES[@]}; do
     if [[ "${SERVICE}" != "default" && "${SERVICE}" != "proxy" ]]; then
-      _wexLog "Installing service... ${SERVICE}"
+      # Clear dir.
+      _wexTestClearTempDir
 
+      # Create a new test app for reference,
+      # it will be used to compare with other test apps after making changes.
+      _appTest_createApp "test-app-ref"
+
+      # Create a new test app if not exists.
+      _appTest_createApp "test-app"
+
+      # Go to fist site
+      cd "${WEX_TEST_DIR_TMP}test-app"
+
+      _wexLog "Installing service... ${SERVICE}"
       wex-exec app::service/install -s="${SERVICE}"
 
       _wexLog "Starting app with new service"
