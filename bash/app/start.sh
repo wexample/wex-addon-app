65c4c9e74195b211a7e2e91c308192d27335e867
weeger <romain.weeger@gmail.com>
2023-02-20 17:18:21 +0100

    Use wex-exec

--- bash/app/start.sh
+++ bash/app/start.sh
@@ -21,28 +21,28 @@ appStart() {
     fi
   fi
 
-  wex prompt::prompt/progress -nl -p=0 -s="Preparing"
+  wex-exec prompt::prompt/progress -nl -p=0 -s="Preparing"
 
   # Stop other sites.
   if [ "${ONLY}" != "" ];then
-    wex app::apps/stop
+    wex-exec app::apps/stop
   fi
 
-  wex prompt::prompt/progress -nl -p=10 -s="Check location"
+  wex-exec prompt::prompt/progress -nl -p=10 -s="Check location"
 
   local DIR
-  DIR=$(wex app::app/locate -d="${DIR}")
+  DIR=$(wex-exec app::app/locate -d="${DIR}")
   # Create env file.
   if [ "${DIR}" = "" ];then
-    if [ "$(wex prompt::prompt/yn -q="No .wex/.env file, would you like to create it ?")" = true ];then
+    if [ "$(wex-exec prompt::prompt/yn -q="No .wex/.env file, would you like to create it ?")" = true ];then
       local APP_ENV;
 
       DIR="$(realpath .)/"
       FILE_ENV="${DIR}${WEX_DIR_APP_DATA}${WEX_FILE_APP_ENV}"
 
-      wex app::prompt/chooseEnv
+      wex-exec app::prompt/chooseEnv
 
-      APP_ENV=$(wex prompt::prompt/choiceGetValue)
+      APP_ENV=$(wex-exec prompt::prompt/choiceGetValue)
 
       if [ -z "$APP_ENV" ]; then
         exit
@@ -59,7 +59,7 @@ appStart() {
     fi
   fi
 
-  if [ "$(wex app::app/started -d="${DIR}")" = "true" ];then
+  if [ "$(wex-exec app::app/started -d="${DIR}")" = "true" ];then
     _wexLog "App already running"
     _appStartSuccess
 
@@ -67,14 +67,14 @@ appStart() {
   fi
 
   local IS_PROXY_SERVER
-  IS_PROXY_SERVER=$(wex service/used -s=proxy)
+  IS_PROXY_SERVER=$(wex-exec service/used -s=proxy)
 
   # Current site is not the server itself.
   if [ "${IS_PROXY_SERVER}" = false ];then
-    wex prompt::prompt/progress -nl -p=30 -s="Check proxy"
+    wex-exec prompt::prompt/progress -nl -p=30 -s="Check proxy"
 
     # The server is not running.
-    if [ "$(wex app::proxy/started)" = false ];then
+    if [ "$(wex-exec app::proxy/started)" = false ];then
       _wexLog "Starting wex server"
       _appStartProxyAndRetry "${PORT}" "${USER}"
       return
@@ -82,7 +82,7 @@ appStart() {
     else
       # Asked port is not the same as currently used.
       if [[ $(_appProxyNeedsRestart) == true ]];then
-        local APPS_COUNT=$(wex apps/list -c);
+        local APPS_COUNT=$(wex-exec apps/list -c);
         # Ignore server itself.
         ((APPS_COUNT--))
 
@@ -93,7 +93,7 @@ appStart() {
         # Restart server with given new port number.
         else
           _wexMessage "Restarting wex server on port ${PORT}"
-          wex app::proxy/stop
+          wex-exec app::proxy/stop
           _appStartProxyAndRetry "${PORT}" "${USER}"
           return
         fi
@@ -117,23 +117,23 @@ appStart() {
   sudo chmod -R g+w .
 
   # Prepare files
-  wex prompt::prompt/progress -nl -p=40 -s="Converting files"
-  wex file/convertLinesToUnix -f="${WEX_FILE_APP_ENV}" &> /dev/null
-  wex file/convertLinesToUnix -f="${WEX_DIR_APP_DATA}" &> /dev/null
+  wex-exec prompt::prompt/progress -nl -p=40 -s="Converting files"
+  wex-exec file/convertLinesToUnix -f="${WEX_FILE_APP_ENV}" &> /dev/null
+  wex-exec file/convertLinesToUnix -f="${WEX_DIR_APP_DATA}" &> /dev/null
 
   # Write new config,
   # it will also export config variables
-  wex prompt::prompt/progress -nl -p=50 -s="Writing configuration"
-  wex app::config/write -s -u="${USER}"
+  wex-exec prompt::prompt/progress -nl -p=50 -s="Writing configuration"
+  wex-exec app::config/write -s -u="${USER}"
 
   if [ ! -s "${WEX_FILEPATH_REL_COMPOSE_BUILD_YML}" ]; then
-     _wexError "Unable to write ${WEX_FILEPATH_REL_COMPOSE_BUILD_YML}" "Try to execute wex config/write to check any write error."
+     _wexError "Unable to write ${WEX_FILEPATH_REL_COMPOSE_BUILD_YML}" "Try to execute wex-exec config/write to check any write error."
      exit
   fi
 
-  wex prompt::prompt/progress -nl -p=60 -s="Registering app"
+  wex-exec prompt::prompt/progress -nl -p=60 -s="Registering app"
   # Reload sites will clean up list.
-  wex apps/reload
+  wex-exec apps/reload
   # Add new site.
   echo -e "\n${DIR}" | tee -a "${WEX_PROXY_APPS_REGISTRY}" > /dev/null
 
@@ -141,7 +141,7 @@ appStart() {
   . "${WEX_FILEPATH_REL_APP_ENV}"
 
   # Rebuild hosts in wex registry.
-  wex app::hosts/update
+  wex-exec app::hosts/update
 
   local OPTIONS=''
   if [ "${CLEAR_CACHE}" = true ];then
@@ -149,20 +149,20 @@ appStart() {
   fi
 
   local OPTIONS_SERVICES
-  OPTIONS_SERVICES=$(wex app::service/exec -c=appStartOptions)
+  OPTIONS_SERVICES=$(wex-exec app::service/exec -c=appStartOptions)
   if [ "${OPTIONS_SERVICES}" != "" ];then
     OPTIONS+="${OPTIONS_SERVICES}"
   fi
 
-  wex prompt::prompt/progress -nl -p=70 -s="Starting container"
+  wex-exec prompt::prompt/progress -nl -p=70 -s="Starting container"
   # Use previously generated yml file.
   docker compose -f "${WEX_FILEPATH_REL_COMPOSE_BUILD_YML}" --env-file "${WEX_FILEPATH_REL_CONFIG_BUILD}" up -d ${OPTIONS}
 
-  wex prompt::prompt/progress -nl -p=80 -s="Updating permission"
-  wex app::app/perms
+  wex-exec prompt::prompt/progress -nl -p=80 -s="Updating permission"
+  wex-exec app::app/perms
 
-  wex prompt::prompt/progress -nl -p=85 -s="Serving app"
-  wex app::app/serve
+  wex-exec prompt::prompt/progress -nl -p=85 -s="Serving app"
+  wex-exec app::app/serve
 
   _appStartSuccess
 }
@@ -191,28 +191,28 @@ _appStartProxyAndRetry() {
   CURRENT_DIR=$(realpath ./)
 
   # Server must be started.
-  wex app::proxy/start -n -p="${PORT}" -u="${USER}"
+  wex-exec app::proxy/start -n -p="${PORT}" -u="${USER}"
 
   # Relaunch manually to be sure to keep given arguments
   cd "${CURRENT_DIR}" || return
-  wex app::app/start "${ARGS}"
+  wex-exec app::app/start "${ARGS}"
 }
 
 _appStartSuccess() {
-  wex prompt::prompt/progress -nl -p=90 -s="Check first initialization"
+  wex-exec prompt::prompt/progress -nl -p=90 -s="Check first initialization"
 
   . "${WEX_FILEPATH_REL_CONFIG_BUILD}"
 
   if [ "${APP_INITIALIZED}" != true ]; then
-    wex prompt::prompt/progress -nl -p=95 -s="Initializing first app launch..."
-    wex app::hook/exec -c=appFirstStartInit
+    wex-exec prompt::prompt/progress -nl -p=95 -s="Initializing first app launch..."
+    wex-exec app::hook/exec -c=appFirstStartInit
 
-    wex app::config/setValue -b -k=APP_INITIALIZED -v=true
+    wex-exec app::config/setValue -b -k=APP_INITIALIZED -v=true
   fi
 
-  wex prompt::prompt/progress -nl -p=100 -s="Started"
+  wex-exec prompt::prompt/progress -nl -p=100 -s="Started"
 
-  wex app::hook/exec -c=appStarted
+  wex-exec app::hook/exec -c=appStarted
 
   # No message for proxy server.
   if [ "${NAME}" = "${WEX_PROXY_NAME}" ];then
@@ -221,7 +221,7 @@ _appStartSuccess() {
 
   echo ""
 
-  local DOMAINS=$(wex app::app/domains)
+  local DOMAINS=$(wex-exec app::app/domains)
   if [ "${DOMAINS}" != "" ];then
     _wexMessage "Your site \"${NAME}\" is up in \"${APP_ENV}\" environment" "You can access to it on these urls : "
 
