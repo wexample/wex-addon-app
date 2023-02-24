bf319e627e8b3a00e31600e54b7415f3d53be290
weeger <romain.weeger@gmail.com>
2023-02-24 18:49:53 +0100

    #66 Logs tail

--- bash/app/logs.sh
+++ bash/app/logs.sh
@@ -4,11 +4,17 @@ appLogsArgs() {
   # shellcheck disable=SC2034
   _ARGUMENTS=(
     'container_name c "Container name suffix like site_name_suffix" false'
+    'tail t "Keep log opened" false'
   )
 }
 
 appLogs() {
   _wexAppGoTo . && . "${WEX_FILEPATH_REL_CONFIG_BUILD}"
 
-  docker logs "$(wex-exec app::app/container -c="${CONTAINER_NAME:-${MAIN_CONTAINER_NAME}}")"
+  local OPTIONS=""
+  if [ "${TAIL}" = "true" ]; then
+    OPTIONS="-f"
+  fi
+
+  docker logs ${OPTIONS} "$(wex-exec app::app/container -c="${CONTAINER_NAME:-${MAIN_CONTAINER_NAME}}")"
 }
