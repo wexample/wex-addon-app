c3e38202e02c8e83e0ec7eaeb04b53874290f98d
weeger <romain.weeger@gmail.com>
2023-02-26 11:50:30 +0100

    SC2068

--- bash/app/domains.sh
+++ bash/app/domains.sh
@@ -11,23 +11,24 @@ appDomainsArgs() {
 
 appDomains() {
   if [ -z "${DIR_SITE+x}" ]; then
-    DIR_SITE=./
+    local DIR_SITE=./
   fi
 
-  local DOCKER_COMPOSE_VARS=($(wex-exec app::config/yml -d=${DIR_SITE}))
+  local DOCKER_COMPOSE_VARS=($(wex-exec app::config/yml -d="${DIR_SITE}"))
   local ALL_DOMAINS=''
 
   if [ -z "${SEPARATOR+x}" ]; then
     local SEPARATOR=" "
   fi
 
-  for DOCKER_COMPOSE_VAR in ${DOCKER_COMPOSE_VARS[@]}; do
-    local DOMAINS=$(sed -n "s/^services_\(.\{0,\}\)_environment_VIRTUAL_HOST\=\"\{0,\}\([^\"]\{0,\}\)\"\{0,\}\$/\2/p" <<<${DOCKER_COMPOSE_VAR})
+  for DOCKER_COMPOSE_VAR in "${DOCKER_COMPOSE_VARS[@]}"; do
+    local DOMAINS
+    DOMAINS=$(sed -n "s/^services_\(.\{0,\}\)_environment_VIRTUAL_HOST\=\"\{0,\}\([^\"]\{0,\}\)\"\{0,\}\$/\2/p" <<<${DOCKER_COMPOSE_VAR})
 
     if [ ! -z "${DOMAINS+x}" ]; then
       # Split multiple domains.
       DOMAINS=($(echo "${DOMAINS}" | tr "," "\n"))
-      for DOMAIN in ${DOMAINS[@]}; do
+      for DOMAIN in "${DOMAINS[@]}"; do
         # Add separator.
         if [ "${ALL_DOMAINS}" != "" ]; then
           ALL_DOMAINS+=${SEPARATOR}
