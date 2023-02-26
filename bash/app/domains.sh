89446943eb832874688cf489898692ecd065aef4
weeger <romain.weeger@gmail.com>
2023-02-26 12:08:10 +0100

    SC2207

--- bash/app/domains.sh
+++ bash/app/domains.sh
@@ -21,13 +21,16 @@ appDomains() {
     local SEPARATOR=" "
   fi
 
+  local IFS_OLD=${IFS}
+  IFS=','
   for DOCKER_COMPOSE_VAR in "${DOCKER_COMPOSE_VARS[@]}"; do
     local DOMAINS
     DOMAINS=$(sed -n "s/^services_\(.\{0,\}\)_environment_VIRTUAL_HOST\=\"\{0,\}\([^\"]\{0,\}\)\"\{0,\}\$/\2/p" <<<${DOCKER_COMPOSE_VAR})
 
-    if [ ! -z "${DOMAINS+x}" ]; then
+    if [ -n "${DOMAINS+x}" ]; then
       # Split multiple domains.
-      DOMAINS=($(echo "${DOMAINS}" | tr "," "\n"))
+      read -ra DOMAINS <<<"${DOMAINS}"
+
       for DOMAIN in "${DOMAINS[@]}"; do
         # Add separator.
         if [ "${ALL_DOMAINS}" != "" ]; then
@@ -37,6 +40,7 @@ appDomains() {
       done
     fi
   done
+  IFS=${IFS_OLD}
 
   echo "${ALL_DOMAINS}"
 }
